93deddd84305bd3ca2718a12fd1f6de822ef06d9
squireaa <squireaa1@gmail.com>
2025-02-08 13:51:19 -0500

    Basic frontent boilerplate

---
+++ frontend/food-map/src/App.js
@@ -0,0 +1,13 @@
+import Map from './components/Map';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <div className='h-96 w-full px-72 m-20 overflow-hidden'>
+        <Map className="" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/food-map/src/components/Map.js
@@ -0,0 +1,25 @@
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+export default function Map() {
+    return (
+        <div>
+            <MapContainer
+                center={[51.505, -0.09]}
+                zoom={13}
+                scrollWheelZoom={false}
+                style={{ height: "100vh", width: "100%" }} // Ensure proper rendering
+            >
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
