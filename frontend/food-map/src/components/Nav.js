43f79a7bbf490a9d599d124e444e299d15254e0a
squireaa <squireaa1@gmail.com>
2025-02-08 15:50:33 -0500

    Basically did nothing so far

---
+++ frontend/food-map/src/components/Nav.js
@@ -0,0 +1,12 @@
+
+
+export default function Nav() {
+    return (
+        <div className="w-full h-48">
+            <ul>
+                <li>Scarcity Map</li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
